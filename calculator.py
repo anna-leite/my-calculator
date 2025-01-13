015040366e5310e00ce5babb8beda36969e34214
anna-leite <anna.leite@laplateforme.io>
2025-01-13 13:26:47 +0100

    "new branch"

---
+++ calculator.py
@@ -0,0 +1,33 @@
+# def number_number():
+#     """Addition of two numbers
+#      Parameters 
+#      ----------
+#       two variables 
+
+#       Returns
+#       -------
+#       a number
+
+#        """
+    
+
+
+
+# def addition(num1, num2):
+#     """Addition of two numbers
+#      Parameters 
+#      ----------
+#       two variables 
+
+#       Returns
+#       -------
+#       a number
+
+#        """
+
+
+num1 = 3
+var_type =  type(num1)
+if var_type == int:
+    print("po")
+    
