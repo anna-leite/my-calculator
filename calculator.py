b30fc13e444ca568ff5e9c7d4eee9d96e10b768d
anna-leite <anna.leite@laplateforme.io>
2025-01-14 13:04:07 +0100

    calculator bases

--- calculator.py
+++ calculator.py
@@ -1,33 +1,46 @@
-# def number_number():
-#     """Addition of two numbers
-#      Parameters 
-#      ----------
-#       two variables 
 
-#       Returns
-#       -------
-#       a number
+# function to input an opération and return a list
+def input_operation(a, b, sign):
+    a = float(input("Enter a number: ", ))
+    sign = input("Enter an operator (+, -, *, /): ", )
+    b = float(input("Enter another number: ", ))
+    return a, b, sign
 
-#        """
+
+
+def addition(a, b):
+    return  a + b
     
+def subtration(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+def calculator(a, b, sign):
+    if sign == "+":
+        print(addition(a, b))
+    elif sign == "-":
+        print(subtration(a, b))
+    elif sign == "*":
+        print(multiplication(a, b))
+    elif sign == "/":
+        print(division(a, b))
+
+def main():
+    a = 0
+    b = 0
+    sign = 0
+    a, b, sign = input_operation(a, b, sign)
+    print(f"{a} {sign} {b} =")
+    calculator(a, b, sign)
 
 
 
-# def addition(num1, num2):
-#     """Addition of two numbers
-#      Parameters 
-#      ----------
-#       two variables 
-
-#       Returns
-#       -------
-#       a number
-
-#        """
+main()
 
 
-num1 = 3
-var_type =  type(num1)
-if var_type == int:
-    print("po")
-    
+
